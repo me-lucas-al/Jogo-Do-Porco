c8ea20fecb2f5d2fccd1f556425a286a1f0ae7be
me-lucas-al <lucasalsouza2006@gmail.com>
2023-12-19 13:08:42 -0300

    Main

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ btnRoll.addEventListener("click", function () {
         const dice = Math.trunc(Math.random() * 6) + 1;
 
         diceEl.classList.remove("hidden");
-        diceEl.src = `dice-${dice}.png`
+        diceEl.src = `/images/dice-${dice}.png`
     
         if (dice !== 1) {
             currentScore += dice;
