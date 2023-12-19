db1fe6d6fb5cb21ea10a7877d42d65acbeb76b4b
me-lucas-al <lucasalsouza2006@gmail.com>
2023-12-19 01:06:04 -0300

    main

---
+++ script.js
@@ -0,0 +1,103 @@
+//Selecionando elementos
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//Condições
+diceEl.classList.add("hidden");
+
+//Funcionalidades do dado
+const scores = [0, 0]
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+}
+btnRoll.addEventListener("click", function () {
+    if(playing){
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        diceEl.classList.remove("hidden");
+        diceEl.src = `dice-${dice}.png`
+    
+        if (dice !== 1) {
+            currentScore += dice;
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
+    }
+});
+
+btnHold.addEventListener("click", function () {
+    if (playing) {
+        scores[activePlayer] += currentScore;
+        document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
+        if (scores[activePlayer] >= 100) {
+            playing = false;
+            diceEl.classList.add("hidden");
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+            document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+        } else {
+            switchPlayer();
+        }
+    }
+});
+
+btnNew.addEventListener("click", function () {
+    playing = true;
+    document.querySelector(`.player--${activePlayer}`).classList.remove("player--winner");
+    activePlayer = 0;
+    currentScore = 0;
+    scores[0] = 0;
+    scores[1] = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    document.querySelector(`.player--${activePlayer}`).classList.add("player--active");
+    btnRoll.addEventListener("click", function () {
+        diceEl.classList.remove("hidden");
+    })
+});
+
+//Modal 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal)
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+})
